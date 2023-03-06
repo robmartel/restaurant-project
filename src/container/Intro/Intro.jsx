206d4d143feca0e8eb748f71f0310142a080fc86
robmartel <99683835+robmartel@users.noreply.github.com>
2023-03-06 17:05:27 -0500

    saved

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -10,25 +10,43 @@ const Intro = () => {
   const handleVideo = () => {
     setplayVideo((prevPlayVideo) => !prevPlayVideo);
 
-    if(playVideo) {
+    if (playVideo) {
       vidRef.current.pause();
     } else {
       vidRef.current.play();
     }
-  }
+  };
 
   return (
-  <div className="app__video">
-    <video src={meal} ref={vidRef} type="video/mp4" loop controls={false} muted />
-    <div className="app__video-overlay flex__center">
-      <div className="app__video-overlay_circle flex__center" onClick={handleVideo}>
-{playVideo ? (
-  <BsPauseFill  color="#FFF" fontSize={30}/> 
-) : <BsFillPlayFill color="#FFF" fontSize={30}/>}
+    <div className='app__video'>
+      <video
+        src={meal}
+        ref={vidRef}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div
+          className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill
+              color='#FFF'
+              fontSize={30}
+            />
+          ) : (
+            <BsFillPlayFill
+              color='#FFF'
+              fontSize={30}
+            />
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
-  }
+  );
+};
 
 export default Intro;
